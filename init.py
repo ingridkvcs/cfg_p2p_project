af5c241a1009a0b7e13b612d178fc7eeef218b0a
big-d0g <alkanet0@protonmail.com>
2022-07-26 13:41:57 +0100

    Switched off sqlalchemy mod tracking as not needed

--- init.py
+++ init.py
@@ -10,6 +10,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{username}:{password}@{host}:{port}/{db_name}'
     db.init_app(app)
     return app
