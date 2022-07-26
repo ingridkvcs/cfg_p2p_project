56f9b43269800508dab76bd3a1e4b5c67b8c3ae6
Ingrid <ingrid_kvcs@yahoo.com>
2022-07-27 00:03:57 +0100

    Added raw data to app.py to test rendering it in my_account.html using jinja.

--- app.py
+++ app.py
@@ -13,9 +13,22 @@ def main_page():
     return render_template("index.html", user_count=users)
 
 
+orders = [
+
+    (1234, 'Lending', 5500, 3.5),
+    (3264, 'Borrowing', 5500, 3.1),
+    (1434, 'Lending', 5500, 3.8),
+    (5237, 'Borrowing', 7500, 5.5),
+    (1838, 'Borrowing', 8800, 2.5),
+    (1234, 'Lending', 5000, 1.5),
+    (1634, 'Borrowing', 6900, 4.5),
+    (6234, 'Lending', 2500, 3.5)
+]
+
+
 @app.route('/my-account')
 def my_account_page():
-    return render_template("my_account.html")
+    return render_template("my_account.html", orders=orders)
 
 
 if __name__ == '__main__':
