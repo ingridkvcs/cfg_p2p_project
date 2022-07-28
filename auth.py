e18295fd858ad5e5edfee39da7c160c85f9ab94e
Ingrid <ingrid_kvcs@yahoo.com>
2022-07-29 00:41:45 +0100

    Added Authentication based on Flask login library
    https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login

---
+++ auth.py
@@ -0,0 +1,70 @@
+from flask import Blueprint, render_template, url_for, redirect, request, flash
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from database.models import User
+from init import db
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # check if the user actually exists
+    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        flash('Email or Password not correct. Try again.')
+        return redirect(url_for('auth.login'))  # if the user doesn't exist or password is wrong, reload the page
+
+    # if the above check passes, then we know the user has the right credentials
+    login_user(user, remember=remember)
+    return redirect(url_for('my_account_page'))
+
+
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # check if a user already exists with this email
+    user = User.query.filter_by(email=email).first()
+
+    # if a user is found, we want to redirect back to signup page so user can try again
+    if user:
+        flash('Email address already exists')
+        return redirect(url_for('auth.signup'))
+
+    # create a new user and with the password encrypted
+    new_user = User(first_name=first_name, last_name=last_name, email=email,
+                    password=generate_password_hash(password, method='sha256'))
+
+    # save the user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main_page'))
--- init.py
+++ init.py
@@ -1,5 +1,4 @@
 # Creates the Flask Application and configures and connects to the database using SQLAlchemy.
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,7 +9,12 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'fCjTw#zFc%sKcyBtU^TS85KY^9NEJFaCKqpv^vV93MPv@RkRCftqmdQAVtTjrsMF'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{username}:{password}@{host}:{port}/{db_name}'
     db.init_app(app)
+
+    from auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
     return app
