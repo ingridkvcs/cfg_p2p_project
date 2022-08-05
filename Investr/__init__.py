c3401940144919c292c5267b4337c55d6e893c41
big-d0g <alkanet0@protonmail.com>
2022-08-05 12:35:27 +0100

    Reconfigured modules into a package (Investr/__init__), fixed bugs in database code

---
+++ Investr/__init__.py
@@ -0,0 +1,23 @@
+# Created "package" for the application so that we are not looping imports (importing the same modules into different files)
+# For new modules please import directly into this file and then import to other files in the app from this file.
+
+import logging
+import csv
+
+from datetime import datetime
+
+from flask import Flask, Blueprint, render_template, url_for, redirect, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
+
+from sqlalchemy.orm import declarative_base, backref, relationship
+from sqlalchemy import Column, Table, ForeignKey, Integer, String, Float, Date
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# User-defined Modules
+
+from database.models import User, OrderBook, Contract, Base
+from database.db_config import db_name, port, host, password, username
+from init import create_app, create_db, db, engine
+from app import app
--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 # Contains all the endpoints for this application
-from flask import render_template
-from flask_login import LoginManager, login_required, current_user
+from Investr import logging
+from Investr import render_template
+from Investr import LoginManager, login_required, current_user
+from Investr import User, OrderBook, Contract
+from Investr import create_app, create_db, db, engine, db_name, csv
 
-from database.models import User
-from init import create_app, db
+logging.basicConfig()
+logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+logging.getLogger("sqlalchemy.pool").setLevel(logging.DEBUG)
 
 app = create_app()
+app.app_context().push()
+
+create_db()
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
-login_manager.init_app(app)
+login_manager.init_app(app) 
 
 
 @login_manager.user_loader
@@ -20,7 +27,7 @@ def load_user(user_id):
 @app.route('/')
 def main_page():
     users = db.session.query(User).count()
-    return render_template("index.html", user_count=users)
+    return render_template("index.html", user_count=users)  
 
 
 headers = ('Order Id', 'Type of Order', 'Amount', 'Interest')
@@ -29,7 +36,7 @@ orders = [
     (1234, 'Lending', 5500, 3.5),
     (3264, 'Borrowing', 5500, 3.1),
     (1434, 'Lending', 5500, 3.8),
-    (5237, 'Borrowing', 7500, 5.5),
+    (5237, 'Borrowing', 7500, 5.5), 
     (1838, 'Borrowing', 8800, 2.5),
     (1234, 'Lending', 5000, 1.5),
     (1634, 'Borrowing', 6900, 4.5),
@@ -50,4 +57,4 @@ def my_account_page():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
