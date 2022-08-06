8f6ed6cb8261099805f8c4de69f47849818f08b6
Ingrid <ingrid_kvcs@yahoo.com>
2022-08-06 12:16:27 +0100

    Minor fix to Order_matching.py

--- Order_matching.py
+++ Order_matching.py
@@ -8,12 +8,12 @@ orders = (['Lending', 1000, 5.15],
           ['Borrowing', 500, 5.1],
           ['Lending', 500, 4.9],
           ['Borrowing', 10000, 6],
-          ['Lending', 20000,4.1])
+          ['Lending', 20000, 4.1])
 
-current_order = ['Borrowing',2500,4.9]
+current_order = ['Borrowing', 2500, 4.9]
 
 
-def Lending(Borrow_list,current_order,Lending_list):
+def Lending(Borrow_list, current_order, Lending_list):
     Total = current_order[1]
     for i in range(len(Borrow_list)):
         if current_order[2] == Borrow_list[i][2]:
@@ -30,11 +30,11 @@ def Lending(Borrow_list,current_order,Lending_list):
 
     Borrow_list = [value for value in Borrow_list if value[1] != 0]
 
-    print(sorted(Borrow_list,key=lambda Borrow_list: Borrow_list[2]))
+    print(sorted(Borrow_list, key=lambda Borrow_list: Borrow_list[2]))
     print(sorted(Lending_list, key=lambda Lending_list: Lending_list[2]))
 
 
-def Borrowing(Lending_list,current_order,Borrow_list):
+def Borrowing(Lending_list, current_order, Borrow_list):
     Total = current_order[1]
     for i in range(len(Lending_list)):
         if current_order[2] == Lending_list[i][2]:
@@ -53,23 +53,21 @@ def Borrowing(Lending_list,current_order,Borrow_list):
     print(sorted(Lending_list, key=lambda Lending_list: Lending_list[2]))
     print(sorted(Borrow_list, key=lambda Borrow_list: Borrow_list[2]))
 
-def order_matching(current_order,orders):
+
+def order_matching(current_order, orders):
     Lend_list = []
-    Borrow_list =[]
+    Borrow_list = []
     for i in range(len(orders)):
         if orders[i][0] == 'Lending':
             Lend_list.append(orders[i])
-        elif orders[i][0] =='Borrowing':
+        elif orders[i][0] == 'Borrowing':
             Borrow_list.append(orders[i])
 
     if current_order[0] == 'Lending':
-        Lending(Borrow_list,current_order,Lending_list)
+        Lending(Borrow_list, current_order, Lend_list)
 
     elif current_order[0] == 'Borrowing':
-        Borrowing(Lend_list,current_order,Borrow_list)
+        Borrowing(Lend_list, current_order, Borrow_list)
 
 
-order_matching(current_order,orders)
-
-
-
+order_matching(current_order, orders)
